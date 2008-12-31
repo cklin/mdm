656412156b6872d93ba83b2e211ed2121f7e5f04
Chuan-kai Lin <cklin@cs.pdx.edu>
2008-12-31 15:42:57 -0800

    Remove timestamps from log messages
    The annotate-output function does an equally good job.

--- mdm-dispatcher.c
+++ mdm-dispatcher.c
@@ -1,4 +1,4 @@
-// Time-stamp: <2008-12-31 14:51:32 cklin>
+// Time-stamp: <2008-12-31 15:40:15 cklin>
 
 #include <sys/socket.h>
 #include <sys/stat.h>
@@ -12,23 +12,11 @@
 #include "bounds.h"
 #include "comms.h"
 
-char *show_time(void)
-{
-  time_t      t;
-  struct tm   tm;
-  static char buffer[9];
-
-  time(&t);
-  localtime_r(&t, &tm);
-  strftime(buffer, 9, "%T", &tm);
-  return buffer;
-}
-
 int main(int argc, char *argv[])
 {
   int    listenfd;
   int    retval, status;
-  int    agent;
+  int    agent, nagents;
   int    commfd[MAX_AGENTS], maxfd;
   bool   busy[MAX_AGENTS];
   pid_t  pid;
@@ -54,54 +42,62 @@ int main(int argc, char *argv[])
   if (log == NULL)  err(3, "Log file %s", logaddr);
   setvbuf(log, NULL, _IONBF, 0);
 
-  for (agent=0, maxfd=0; agent<MAX_AGENTS; agent++) {
-    fprintf(log, "%s [%d] waiting... ", show_time(), agent);
-    commfd[agent] = serv_accept(listenfd);
-    read(commfd[agent], &pid, sizeof (pid_t));
-    fprintf(log, "online! pid=%d\n", pid);
-    if (commfd[agent] > maxfd)  maxfd = commfd[agent];
-    busy[agent] = false;
-  }
-  close(listenfd);
-
   char  *exec_argv[] =
     { "/usr/bin/lame", "--resample", "22.05",
       "-m", "m", "-V", "6", file, NULL };
 
+  nagents = 0;
   for ( ; ; ) {
-    for (agent=0; agent<MAX_AGENTS; agent++) {
+    for (agent=0; agent<nagents; agent++) {
       if (busy[agent])  continue;
       if (!fgets(file, MAX_ARG_SIZE, stdin))  continue;
       if (file[strlen(file)-1] == '\n')
         file[strlen(file)-1] = '\0';
-      fprintf(log, "%s [%d] %s\n", show_time(), agent, file);
+      fprintf(log, "[%d] %s\n", agent, file);
       write_cmd(commfd[agent], exec_argv);
       busy[agent] = true;
     }
 
-    for (agent=0; agent<MAX_AGENTS; agent++)
+    for (agent=0; agent<nagents; agent++)
       if (busy[agent])  break;
-    if (agent == MAX_AGENTS)  break;
+    if (nagents > 0 && agent == nagents)  break;
 
     FD_ZERO(&readfds);
-    for (agent=0; agent<MAX_AGENTS; agent++)
+    FD_SET(listenfd, &readfds);
+    for (agent=0, maxfd=listenfd; agent<nagents; agent++) {
       FD_SET(commfd[agent], &readfds);
+      if (commfd[agent] > maxfd)  maxfd = commfd[agent];
+    }
     retval = select(maxfd+1, &readfds, NULL, NULL, NULL);
     if (retval < 0)  err(4, "select");
 
-    for (agent=0; agent<MAX_AGENTS; agent++)
+    for (agent=0; agent<nagents; agent++)
       if (FD_ISSET(commfd[agent], &readfds)) {
         if (!busy[agent])
           errx(5, "agent %d protocol error", agent);
         read(commfd[agent], &status, sizeof (int));
-        fprintf(log, "%s [%d] done, status %d\n",
-                show_time(), agent, status);
+        fprintf(log, "[%d] done, status %d\n", agent, status);
         busy[agent] = false;
       }
+
+    if (FD_ISSET(listenfd, &readfds)) {
+      int comm;
+
+      comm = serv_accept(listenfd);
+      if (nagents == MAX_AGENTS) {
+        close(comm);
+        continue;
+      }
+      commfd[nagents] = comm;
+      read(commfd[nagents], &pid, sizeof (pid_t));
+      fprintf(log, "[%d] online! pid=%d\n", nagents, pid);
+      busy[nagents] = false;
+      nagents++;
+    }
   }
 
   status = 0;
-  for (agent=0; agent<MAX_AGENTS; agent++)
+  for (agent=0; agent<nagents; agent++)
     write(commfd[agent], &status, sizeof (int));
 
   return 0;
