8988f0c1d30600ffdfe80dbf0b73506da8e5317f
Chuan-kai Lin <cklin@cs.pdx.edu>
2009-01-06 21:26:40 -0800

    Record both fd and pid in worker struct

--- mdm-dispatcher.c
+++ mdm-dispatcher.c
@@ -1,4 +1,4 @@
-// Time-stamp: <2009-01-06 21:10:10 cklin>
+// Time-stamp: <2009-01-06 21:26:18 cklin>
 
 #include <assert.h>
 #include <sys/socket.h>
@@ -17,24 +17,30 @@
 
 extern char **environ;
 
-static int workers[MAX_WORKERS];
+struct worker {
+  int   fd;
+  pid_t pid;
+};
+
+static struct worker workers[MAX_WORKERS];
 static int ready;
 
 #define IS_READY(x) (0 <= (x) && (x) < ready)
 
 void worker_swap(int w1, int w2)
 {
-  int temp;
+  struct worker temp;
 
   temp = workers[w1];
   workers[w1] = workers[w2];
   workers[w2] = temp;
 }
 
-void worker_init(int fd)
+void worker_init(int fd, pid_t pid)
 {
   assert(ready < MAX_WORKERS);
-  workers[ready++] = fd;
+  workers[ready].fd = fd;
+  workers[ready++].pid = pid;
 }
 
 void worker_exit(int w)
@@ -51,7 +57,7 @@ void issue(int widx)
   const int zero = 0, one = 1;
   char      file[MAX_ARG_SIZE];
   int       last;
-  int       worker_fd = workers[widx];
+  int       worker_fd = workers[widx].fd;
   char      *cwd;
   char      *cmd[] =
     { "/usr/bin/lame", "--resample", "22.05",
@@ -75,7 +81,7 @@ void issue(int widx)
   write_args(worker_fd, (const char **) environ);
   free(cwd);
 
-  fprintf(log, "[%d] %s\n", widx, file);
+  fprintf(log, "[%5d] %s\n", workers[widx].pid, file);
 }
 
 int main(int argc, char *argv[])
@@ -112,21 +118,23 @@ int main(int argc, char *argv[])
     FD_ZERO(&readfds);
     FD_SET(listenfd, &readfds);
     for (widx=0, maxfd=listenfd; widx<ready; widx++) {
-      FD_SET(workers[widx], &readfds);
-      if (workers[widx] > maxfd)  maxfd = workers[widx];
+      FD_SET(workers[widx].fd, &readfds);
+      if (workers[widx].fd > maxfd)  maxfd = workers[widx].fd;
     }
     if (select(maxfd+1, &readfds, NULL, NULL, NULL) < 0)
       err(4, "select");
 
     for (widx=ready-1; widx>=0; widx--)
-      if (FD_ISSET(workers[widx], &readfds)) {
+      if (FD_ISSET(workers[widx].fd, &readfds)) {
         int status;
-        if (readn(workers[widx], &status, sizeof (int))) {
-          fprintf(log, "[%d] done, status %d\n", widx, status);
+        if (readn(workers[widx].fd, &status, sizeof (int))) {
+          fprintf(log, "[%5d] done, status %d\n",
+                  workers[widx].pid, status);
           issue(widx);
         }
         else {
-          fprintf(log, "[%d] lost connection\n", widx);
+          fprintf(log, "[%5d] lost connection\n",
+                  workers[widx].pid);
           worker_exit(widx);
         }
       }
@@ -138,9 +146,9 @@ int main(int argc, char *argv[])
         close(new_fd);
       else {
         pid_t pid;
-        worker_init(new_fd);
         readn(new_fd, &pid, sizeof (pid_t));
-        fprintf(log, "[%d] online! pid=%d\n", ready, pid);
+        worker_init(new_fd, pid);
+        fprintf(log, "[%5d] online!\n", pid);
         issue(ready-1);
       }
     }
