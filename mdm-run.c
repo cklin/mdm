7526a4b8dc7d5a6d1bfe87403f4bf8fe72e3478b
Chuan-kai Lin <cklin@cs.pdx.edu>
2009-01-31 20:36:26 -0800

    Inline write_cmd function

--- mdm-run.c
+++ mdm-run.c
@@ -1,4 +1,4 @@
-// Time-stamp: <2009-01-31 19:11:34 cklin>
+// Time-stamp: <2009-01-31 20:36:03 cklin>
 
 #include <sys/stat.h>
 #include <err.h>
@@ -9,15 +9,25 @@
 
 extern char **environ;
 
-void write_cmd(char *argv[])
+int main(int argc, char *argv[])
 {
+  struct stat exe_stat;
   const int   one = 1;
   char        *cwd, *core_addr;
   int         core_fd;
   struct stat sock_stat;
 
+  if (argc < 2)
+    errx(1, "Please supply command as arguments");
+  if (stat(*(++argv), &exe_stat) < 0)
+    err(2, "Cannot stat executable %s", *argv);
+  if (!S_ISREG(exe_stat.st_mode))
+    errx(3, "%s: Not a regular file", *argv);
+
   core_addr = getenv("MDM_CMD_SOCK");
-  if (!core_addr)  return;
+  if (!core_addr)
+    if (execve(*argv, argv, environ) < 0)
+      errx(8, "execve: %s", *argv);
 
   if (lstat(core_addr, &sock_stat) < 0)
     err(4, "%s: Cannot stat core socket", core_addr);
@@ -36,24 +46,6 @@ void write_cmd(char *argv[])
   write_args(core_fd, (const char **) argv);
   write_args(core_fd, (const char **) environ);
   close(core_fd);
-
-  exit(0);
-}
-
-int main(int argc, char *argv[])
-{
-  struct stat exe_stat;
-
-  if (argc < 2)
-    errx(1, "Please supply command as arguments");
-  if (stat(*(++argv), &exe_stat) < 0)
-    err(2, "Cannot stat executable %s", *argv);
-  if (!S_ISREG(exe_stat.st_mode))
-    errx(3, "%s: Not a regular file", *argv);
-
-  write_cmd(argv);
-  if (execve(*argv, argv, environ) < 0)
-    errx(8, "execve: %s", *argv);
 
   return 0;
 }
