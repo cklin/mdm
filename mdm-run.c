e00a3cff94462f5830f73e36576eebd4f534f488
Chuan-kai Lin <cklin@cs.pdx.edu>
2009-02-07 10:36:06 -0800

    Rename core_ to master_

--- mdm-run.c
+++ mdm-run.c
@@ -1,4 +1,4 @@
-// Time-stamp: <2009-02-06 22:57:55 cklin>
+// Time-stamp: <2009-02-07 10:35:33 cklin>
 
 #include <sys/stat.h>
 #include <err.h>
@@ -12,36 +12,36 @@ extern char **environ;
 int main(int argc, char *argv[])
 {
   struct stat sock_stat;
-  char        *core_addr;
-  int         core_fd, status;
+  char        *master_addr;
+  int         master_fd, status;
   job         job;
 
   if (argc < 2)
     errx(1, "Please supply command as arguments");
 
-  core_addr = getenv(CMD_SOCK_VAR);
-  if (!core_addr)
+  master_addr = getenv(CMD_SOCK_VAR);
+  if (!master_addr)
     if (execvp(*argv, ++argv) < 0)
       errx(8, "execve: %s", *argv);
 
-  if (lstat(core_addr, &sock_stat) < 0)
-    err(4, "%s: Cannot stat core socket", core_addr);
+  if (lstat(master_addr, &sock_stat) < 0)
+    err(4, "%s: Cannot stat master socket", master_addr);
   if (!S_ISSOCK(sock_stat.st_mode))
-    errx(5, "%s: Not a socket", core_addr);
+    errx(5, "%s: Not a socket", master_addr);
   if (sock_stat.st_uid != geteuid())
-    errx(6, "%s: Belongs to someone else", core_addr);
+    errx(6, "%s: Belongs to someone else", master_addr);
 
-  core_fd = cli_conn(core_addr);
-  if (core_fd < 0)
-    errx(7, "%s: cli_conn error", core_addr);
+  master_fd = cli_conn(master_addr);
+  if (master_fd < 0)
+    errx(7, "%s: cli_conn error", master_addr);
 
-  write_int(core_fd, 1);
+  write_int(master_fd, 1);
   job.cwd = get_current_dir_name();
   job.cmd.svec = ++argv;
   job.env.svec = environ;
-  write_job(core_fd, &job);
-  readn(core_fd, &status, sizeof (int));
-  close(core_fd);
+  write_job(master_fd, &job);
+  readn(master_fd, &status, sizeof (int));
+  close(master_fd);
 
   return 0;
 }
